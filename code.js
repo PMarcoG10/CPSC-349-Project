584805c9e1b4c76b76fc82c053fe088c9f53fae3
PMarcoG10 <marcog10@csu.fullerton.edu>
2023-03-10 12:01:21 -0800

    Updated

--- code.js
+++ code.js
@@ -1,5 +1,13 @@
 /*This functions changes the background image when the button is pressed*/
+var isBackground = true;
+
 function changeBackground() {
-// .style.backgroundImage = "url(/images/nightsky_lofi.jpg)"
-    document.getElementById("img").style.backgroundImage = "url(/images/nightsky_lofi.jpg)"
+    var images = document.querySelector('body');
+    if (isBackground) {
+        images.style.backgroundImage = "url(/images/nightsky_lofi.jpg)";
+        isBackground = false;
+    } else {
+        images.style.backgroundImage = "url(/images/daytime_lofi.jpg)";
+        isBackground = true;
+    }
 }
