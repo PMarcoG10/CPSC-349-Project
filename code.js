e77b3245dffc52a21943ec36cffcd6adb2cfe296
PMarcoG10 <marcog10@csu.fullerton.edu>
2023-03-10 11:29:18 -0800

    Updated

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
 /*This functions changes the background image when the button is pressed*/
-function ChangeImg() {
-    ddocument.body.style.backgroundImage="url(/images/nightsky_lofi.jpg)";
+function changeBackground() {
+// .style.backgroundImage = "url(/images/nightsky_lofi.jpg)"
+    document.getElementById("img").style.backgroundImage = "url(/images/nightsky_lofi.jpg)"
 }
